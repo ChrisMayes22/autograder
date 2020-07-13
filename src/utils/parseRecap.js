369980a4fa6dc11cdc587b8a837e11d9697729e7
Chris Mayes <coachchristac@gmail.com>
2020-07-12 22:34:40 -0400

    Added new test

--- src/utils/parseRecap.js
+++ src/utils/parseRecap.js
@@ -1,72 +1,76 @@
 
 const testString = `
 
-Section 4: Science (35 Minutes—40 Questions | 6 Passages)
+Section 3: Reading
 
-Passage 1: Whale Feeding Speed/Gape Angle during 5 Stages 
+Section 4: Science
+Table describes three phases of a muscle twitch. Figure 1 shows the force in Newtons (N) of a muscle twitch over time in seconds.
+(B) Detail- from ~0-87 increasing, after ~87 decreasing. Answer b only answer that includes before and after 87.
+(F) Inference- latent:~0-2.5 seconds, contraction: ~2.5-87 seconds, relaxation: ~87-250 seconds by using data on graph one and the zoom in for latent 
+(B) Inference- The maximum of the curve is ~90 seconds after initial twitch time
+(G) New Information- The max is ~7.5 N, thus twice would be ~15N
+(C) Detail- notice wording “before” or “after”. If student picked A, reading too fast 
+()
+(P)assage #2 
+()
+()
+(G) Detail- looking at dotted line in figure 2 where 03 is zero. The range is above 18 and below 22 so G is correct.
+(B) Detail- September is bold line, has a max 03 at 7. November is dashed line, has a max 03 at 20.
+(J) Inference- at about 25km, the lines all converge. Thus, there is very minimal variation in air temp at 25km.
+(A) Inference- passage says 03 content decreases from August and reaches a low around October 15. October 1 would thus have more 03 than the amount graphed on October 15 (.5mpa). However, it wouldn’t be drastically larger (over 3.5 mpa difference), so answer A makes most sense.
+(H) Inference/Outside Info- Can eliminate F and G because they are not true based off figure 2 data. Must be able to infer less 03 in stratosphere means less interference of ultraviolet radiation rays coming in. Thus, H correct because greater UV ray intensity will result from less 03.
+()
+(P)assage #3
+()
+(C) Detail- only student 4 describes the beaker initially containing only a liquid. The other three say there was a solute mixed in as well. 
+(J) Outside Info/Detail- he said the liquid evaporated. Must know evaporation is the term used to describe the state change from liquid to gas. 
+(A) Detail/Outside info- teacher describes solid floating, thus density is less than the liquid. 
+(G) Detail- student 1 and 4 both describe the beaker containing a “pure green solvent”
+(D) Detail- As previously mentioned, student 1 describes a beaker containing a “pure green solvent” while student three describes the solvent only turning green after the solute (solid) is dissolved in the solvent. 
+(J) New Information- students 1-3 say the exact same thing. Student 4 only one who doesn’t mention a solid concentration increasing or a liquid concentration decreasing (evaporating)
+(B) New Information- student two describes a green colored solute-solvent complex (SSC) as a result of mixing a white solid with a colorless liquid.  
+()
+(P)assage #4
+()
+(J) Detail- Experiment 2 graph shows group 7 and 8 with largest decrease after 1 day of exposure. The following days have little to no drop. 
+(B) Detail- dosage is group 2-8 in table 1. On figure 2, for the 10th day after exposure, the graph shows a decreasing percent of survival from group 2-8. 
+(F) Detail/Inference- In figure 1, looking at lined bars (FT) which correspond to UV-B exposure. Only H. regilla species had over 95% hatched (less than 5% mortality).
+(B) Detail- Figure 3, H. regilla bar larger than B. boreas bar, which indicates higher rate of CBPD removal.
+(G) New information- want to subtract to find the difference between filter and no filter eggs
+(A) New information- FB blocked UVB, FT transmitted UVB. A is only correct description of experiment 1 data. 
+()
+(P)assage #5 
+()
+(F) Detail- RH (relative humidity) is the measure of moisture content in air. Tank 1 has lowest RH. 
+(C) New Information- We have data for aqueous tension where RH is 96 and 75. For KCl, RH is between 96 and 75 so aqueous tension is between 110 and 140.
+(G) New Information- From experiments 1-3 temp is increasing, so analyzing if salts 3,4,and 5 have decreasing RH over experiments 1-3. 
+(D) New Information- between experiments 2 and 3, only tank 2 and 5 have the same RH but neither have the same aqueous tension, thus can eliminate A and B. C is just incorrect- tank 2 and 3 are not the same.
+(F) Inference- first procedure is “a drying agent was placed in each tank”. F is only answer that logically explains why.
+(D) Outside Info/Inference- Scientists change an independent variable to measure the results of the dependent variable that is dependent on the independent variable being changed.
+()
+(P)assage #6
+()
+(J) Detail- lowest density is ethanol so should be first in list which eliminates all choices except J.
+(C) Inference- Figure shows trend that density and Pl are directly proportional. Thus, if D is kept constant, then Pl will be twice as large for Carbon tetrachloride than ethanol since its density is twice as large.
+(H) trend
+(A) method
+(H) trend
+()
+(P)assage #7
+(C) detail
+(G) method
+(C) trend
+(H) detail
+(A) method
+(F) trend
 
-(C) Detail → Focus on the gape angle, not the speed! If focused on wrong curve, will miss #1, #2, & #3 
-(H) Detail → The figure shows the speed decreasing rapidly leading up to stage 3. We also see that the gape angle (values are shown on the right side of the graph) is the highest point. (75 degrees). 
-(A) Detail Careful not to rush here! There is nothing going on at 6 seconds if looking at right curve! 
-(F) Detail → Make sure to look at the dashed line! The entire event happened between 10 seconds and 16 seconds, so the event happened in 6 seconds total. 
-(D) Detail/Outside Information → Focus on stage one only! The whale is moving the fastest at 8 seconds. Need to know that greatest momentum will be at greatest speed) 
-(G) Trend → During stage two, the angle of the whale’s mouth widens from zero degrees to 75 degrees. 
 
-Passage 2: Amusement park ride- acceleration and velocity 
-
-(D) Detail →  see figure 2. The height at ground level is 0 and the cart never reaches ground level according to the height (m) graph.
-(J) Detail → must look at acceleration graph. If a student does not look closely, they may not see that the line has passed the halfway mark between 10 and 20. If going too fast G can be an appealing answer.
-(C) Inference → the height changes from around 43 on trip one to around 38 on trip two. We can infer that the third trip will safely fall in between 30 and 40m
-(F) Detail → must look closely at where the lines intersect, as the answer choices are very close to each other 
-(C) Detail →  Point P is at the very top according to initial info in the passage(“cart released from rest at Point P”). If the question is not read carefully, B will be an appealing answer as the cart is 40 feet above ground level.  
-(F) Detail/Outside Information →  the gravitational potential energy is greatest before the ride/cart has launched
-
-Passage 3: Southern Pine Beetles (SPBs), Fungus, Bacteria, two experiments. Students should understand that the graphs are showing inhibition, not growth
-
-(A) Detail → Differences between experiment 1 and 2
-(H) Method → measuring both bacteria and fungi, otherwise, the experiments would not make sense
-(D) Detail→  highest DOI is mild (Experiment 2 → Strain R)
-(G) Detail →  if your initial skim flags where ZOI is referenced then you can quickly get back to the important info - ZOI “the space between the edges of fungal growth and bacteria growth” 
-(C) Detail → refer to Table 1 & Figure 1 
-(J) Trend → use table one and the key to find strongest inhibition and %.
-(A) Method →  just focus on what is in Figure 1 - Fungus M & A and Strain W 
-
-Passage 4: Lots going on here, need to be very efficient with time (will be hard for most students to finish this one in 6 mins or less). Make sure students understand independent/dependent variables in each experiment. Experiment 1 used iron for all 12 trials, Experiment 2 copies procedures for Trial 5 in Experiment 1 but varied core, and Experiment 3 copies procedures from Trial 1-12 but uses direct current battery (instead of alternating current used in Experiment 1) 
-
-(H) Inference → In trials 1-8, &in and Iin serve as constants. You have to look at trials 9-12 to establish the relationship between the two values (as &in increases also increases)
-(A) Inference →  As Nin increases, &out decreases. &out is 6 when Nin is 250. If we further increase Nin to 300, the value of &out will fall below 6V. 
-(G) New Information →   Students need to realize that the &out values are on the left side of the graph and the Iout values are on the left side. &out for K is 60, and Iout is 50. This corresponds to trial 5 in experiment 1. Just based off this one value it has to be answer G . 
-(B) New Information → focus on trial 8. Nin=50, Nout of 300 is closest to the Nout of 250 given in the problem, and &in=30. The values in B most closely reflect the remaining values in trial 8. 
-(F) Detail→  H and J can not be true, since experiment 3 (the only one with DC current) has an Eout voltage equal to zero. F is correct because there were &out voltages for trials with AC (experiment 1 and 2) and no voltages for trials with DC (experiment 3)
-(D) Detail →  In experiment one they use an iron core and AC current (eliminating choice A) Experiment 3 tells us that trials 1-12 were repeated (iron core) except that they used DC current (eliminating B) Experiment three tells us trial 5 was repeated (AC power source), except they changed the material (including non-metal materials like glass, eliminating C.) There is no instance of the non-metal substances from table 2 being used with DC current.
-(F) Detail →  The explanation for experiment 2 tells us that the procedure for trial 5 was repeated, so for trials 13-17 we have the same values as trial 5 in experiment one (Nin = 50, Nout = 100, and &in = 30)
-
-
-
-Passage 5: snowball earth, three experiments (no charts/graphs so this one takes the most time!!!) It is important for students to realize that the smaller value times are more recent (690 million years ago is more recent than 700 mya.)
-
-(D) Detail →  use info given in question, if form Fe203 need that to be in the product formed by Iron & Oxygen 
-(G) Detail →  scientist 2 compared with scientist 1, Scientist 2 says add CO2 while Scientist 1 only discusses removal of CO2 
-(B) Trend →  Key words: “continual” → continual tells us that the CO2 is added to the atmosphere at a constant rate. 
-(H) New Information/Trend →  “photosynthetic organisms” want the answer choice that is closest to the equator 
-(B) New Information → (*suppose=new info) Has to be scientist 1 b/c scientist 2  says the temperature has to be above 0) → B is correct b/c the -10 is below the maximum temperature at which ice can form
-(J) New Information → the position would be weakened, because Scientist 1 claimed the BIFs formed by sudden contact of the the iron ions with oxygen in the million years after 700mya (700-699mya). If no oxygen was present from 700-690mya, no BIFs could be formed.
-(C) New Information  → It would contradict both scientists. Both Scientist 1 (mass extinction end of paragraph 2) and 2’s (entire earth covered except lower latitudes start of paragraph 3) claims require that 60N be covered in ice at 705 mya, like the New Information suggests.  
-
-Passage 6: three experiments dealing with ideal gas law (IGL) 
-
-(H) Detail →  T was not held constant during Experiment 1 (T ranged from 200 K to 450 K in Experiment 1)
-(B) Method → we are told in the description of experiment 2 that T is held constant at 400K and that our volume is fixed at 1.0 liter (L). The graph shows us changing values for n and P.
-(J) Trend → The solid line represents an ideal gas, which Z deviates the least from
-(D) Outside information →  need to know the relationship between pressure, temperature, and energy. Pressure of a gas is the number of collisions between the gas particles and the container. An increase in pressure means more collisions, a higher temperature, and more energy. The pressure at 4.0 MPa would cause an increase in both the temperature and kinetic energy.
-(J) Method/Trend →  (use elimination → because of Experiment 2, all other answer choices are incorrect because the MPA is 100)
-(C) New Information→  Experiment 2 is focus, Gas Y weighs 5g at 25 MPa but need to multiply that by 20g to get answer → 20x5=100 (B is most common wrong answer...attention to detail!) 
-(F) New Information  →  if you decrease one side, decrease the other for consistency in results 
 
 `
 
 function parseDebrief(debrief){
     splitDebrief = debrief.replace( /\n/g, " " ).replace("  ", " ").split( " " )
+    console.log(splitDebrief)
     result = []
     answers = ['(A)', '(B)', '(C)', '(D)', '(E)', '(F)', '(G)', '(H)', '(J)', '(K)']
     for (let i = 0; i < splitDebrief.length; i++){
@@ -76,8 +80,80 @@ function parseDebrief(debrief){
             splitDebrief[i+1].toLowerCase()])
         }
     }
-    result
     return result
 }
 
-console.log(parseDebrief(testString))
+scale = [
+    [ 'B', 'backsolve:' ],
+            [ 'D', 'simplify:' ],
+            [ 'C', 'backsolve:' ],
+            [ 'B', 'simplify:' ],
+            [ 'E', 'backsolve:' ],
+            [ 'B', 'simplify:' ],
+            [ 'C', 'simplify/eliminate:' ],
+            [ 'E', 'pick smart numbers' ],
+            [ 'C', 'elimination:' ],
+            [ 'C', 'elimination:' ],
+            [ 'A', 'elimination/simplify:' ],
+            [ 'E', 'simplify:' ],
+            [ 'C', 'eliminate/simplify:' ],
+            [ 'B', 'simplify:' ],
+            [ 'C', 'simplify/eliminate:' ],
+            [ 'E', 'pick smart numbers' ],
+            [ 'D', 'simplify:' ],
+            [ 'B', 'simplify:' ],
+            [ 'D', 'simplify:' ],
+            [ 'A', 'simplify:' ],
+            [ 'A', 'eliminate:' ],
+            [ 'E', 'use proportions' ],
+            [ 'D', 'eliminate/use' ],
+            [ 'C', 'simplify:' ],
+            [ 'E', 'simplify/eliminate:' ],
+            [ 'B', 'simplify:' ],
+            [ 'C', 'simplify:' ],
+            [ 'E', 'simplify:' ],
+            [ 'C', 'pick smart numbers' ],
+            [ 'D', 'elimination:' ],
+            [ 'C', 'elimination:' ],
+            [ 'A', 'eliminate:' ],
+            [ 'E', 'eliminate/simplify:' ],
+            [ 'A', 'elimination:' ],
+            [ 'C', 'use proportions' ],
+            [ 'B', 'simplify:' ],
+            [ 'B', 'eliminate:' ],
+            [ 'B', 'simplify:' ],
+            [ 'E', 'simplify:' ],
+            [ 'D', 'simplify:' ],
+            [ 'C', 'simplify:' ],
+            [ 'A', 'simplify/elimination:' ],
+            [ 'E', 'simplify:' ],
+            [ 'C', 'backsolve:' ],
+            [ 'A', 'simplify:' ],
+            [ 'D', 'simplify:' ],
+            [ 'D', 'pick smart numbers' ],
+            [ 'B', 'elimination:' ],
+            [ 'A', 'simpliy/backsolve:' ],
+            [ 'B', 'simplify:' ],
+            [ 'A', 'simplify:' ],
+            [ 'D', 'backsolve/simplify:' ],
+            [ 'D', 'simplify:' ],
+            [ 'E', 'eliminate:' ],
+            [ 'A', 'pick smart numbers' ],
+            [ 'E', 'simplify:' ],
+            [ 'D', 'elimination:' ],
+            [ 'D', 'simplify:' ],
+            [ 'A', 'elimination:' ],
+            [ 'E', 'simplify:' ]
+  ]
+
+for (i = 0; i< scale.length; i++){
+    if(i < 21){
+        scale[i][1] = "1-20"
+    } else if (i < 41){
+        scale[i][1] = "21-40"
+    } else {
+        scale[i][1] = "41-60"
+    }
+    console.log(scale[i])
+}
+
