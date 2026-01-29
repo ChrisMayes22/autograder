d73e67f4503f041927517f034b5476aec1bfaf05
Christopher Mayes <maddielemons@Christophers-MacBook-Air.local>
2026-01-29 11:54:41 -0500

    Fixed grading to ignore science for composite

--- src/utils/graders/getActScore.js
+++ src/utils/graders/getActScore.js
@@ -14,22 +14,15 @@ function getActScore(studentAnswers, test, goals, timing, guesses){
             scoreReport[subject].goal = goals[subject]
             scoreReport[subject].timing = timing[subject]
             scoreReport[subject].guesses = guesses[subject]
-            composite += scoreReport[subject].score
+            if(subject !== 'science'){
+                composite += scoreReport[subject].score
+            }
         }
-    
     })
 
-    
-    if(scoreReport.science){
-        composite = Math.round(composite / 4)
-        scoreReport.composite = {score: composite}
-        scoreReport.composite.goal = ( scoreReport.english.goal + scoreReport.math.goal + scoreReport.reading.goal + scoreReport.science.goal ) / 4
-    } else {
-        composite = Math.round(composite / 3)
-        scoreReport.composite = {score: composite}
-        scoreReport.composite.goal = ( scoreReport.english.goal + scoreReport.math.goal + scoreReport.reading.goal) / 3
-    }
-
+    composite = Math.round(composite / 3)
+    scoreReport.composite = {score: composite}
+    scoreReport.composite.goal = ( scoreReport.english.goal + scoreReport.math.goal + scoreReport.reading.goal) / 3
     
     console.log('SCORE REPORT', scoreReport)
 
